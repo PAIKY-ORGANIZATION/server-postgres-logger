a4047951fcd2fa2835974194270871ee516a1910
Priapisman677 <expeliamm02@hotmail.com>
2025-07-21 10:28:01 -0700

    update

--- src/app-listens.ts
+++ src/app-listens.ts
@@ -2,7 +2,7 @@ import app from "./app-setup.js";
 
 
 //* Start Express server
-const port = process.env.PORT 
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
     console.log('Server started on port ' + port);
